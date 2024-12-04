2dd19923b4fafb3e41e4c6d3b2d1e606d35870e6
martian58 <fuadelizade6@gmail.com>
2024-12-04 14:03:12 +0400

    Update the average calculator

---
+++ test.py
@@ -0,0 +1,67 @@
+# Math
+note_book_coef = (6/6)*1
+first_written_exam_coef = (6/6)*2
+second_written_exam_coef = (6/6)*3
+
+# Language
+lang_1_coef = (3/8)*2
+lang_2_coef = (3/8)*3
+lang_3_coef= (3/8)*3  
+
+# Physics
+fluid_coef = (6/5)*1
+pm_coef = (6/5)*2
+termo_coef = (6/5)*2
+
+# Chemistry
+aom_first_written_exam_coef = (2/3)*1
+aom_second_written_exam_coef = (2/3)*2
+
+lab_coef = 2  
+
+# Computer Science
+front_pw_coef = (11/4)*0.1
+front_project_coef = (11/4)*0.4
+front_exam_coef = (11/4)*0.5
+
+dsa_pw_project_coef = (11/4)*2*0.5
+dsa_exam_coef = (11/4)*2*0.5
+
+cspc_pw_coef = (11/4)*0.4
+cspc_first_exam_coef = (11/4)*0.2
+cspc_second_exam_coef = (11/4)*0.4
+
+# Print the results
+print("Math Coefficients:")
+print(f"Note Book Coefficient: {note_book_coef}")
+print(f"First Written Exam Coefficient: {first_written_exam_coef}")
+print(f"Second Written Exam Coefficient: {second_written_exam_coef}")
+print()
+
+print("Language Coefficients:")
+print(f"Language 1 Coefficient: {lang_1_coef}")
+print(f"Language 2 Coefficient : {lang_2_coef}")
+print(f"Language 3 Coefficient : {lang_3_coef}")
+print()
+
+print("Physics Coefficients:")
+print(f"Fluid Coefficient: {fluid_coef}")
+print(f"PM Coefficient: {pm_coef}")
+print(f"Termo Coefficient: {termo_coef}")
+print()
+
+print("Chemistry Coefficients:")
+print(f"AOM First Written Exam Coefficient: {aom_first_written_exam_coef}")
+print(f"AOM Second Written Exam Coefficient: {aom_second_written_exam_coef}")
+print(f"Lab Coefficient: {lab_coef}")
+print()
+
+print("Computer Science Coefficients:")
+print(f"Front PW Coefficient: {front_pw_coef}")
+print(f"Front Project Coefficient: {front_project_coef}")
+print(f"Front Exam Coefficient: {front_exam_coef}\n")
+print(f"DSA PW Project Coefficient: {dsa_pw_project_coef}")
+print(f"DSA Exam Coefficient: {dsa_exam_coef}\n")
+print(f"CSPC PW Coefficient: {cspc_pw_coef}")
+print(f"CSPC First Exam Coefficient: {cspc_first_exam_coef}")
+print(f"CSPC Second Exam Coefficient: {cspc_second_exam_coef}")

41c88d3d4e112a1d4076c8da0a184665ab2d7ac4
martian58 <fuadelizade6@gmail.com>
2025-01-08 13:42:49 +0400

    fav

--- app.py
+++ app.py
@@ -237,6 +237,12 @@ def execute_command():
         return jsonify({"success": False, "message": "Error executing the command"}), 500
 
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
 if __name__ == '__main__':
 
     app.run(debug=True, port=5050)
